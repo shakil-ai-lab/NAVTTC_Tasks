7c000da2f66561ea69527a13343e703930d2b252
Shakil Ur Rehman <sanjaralap@gmail.com>
2026-05-05 07:38:01 +0500

    add sqrt.py

---
+++ calculate_sqrt.py
@@ -0,0 +1,8 @@
+# calculate square root of a number by taking input from the user
+num = float(input("Enter a number: "))
+
+# calculating the square root
+sqrt = num ** 0.5
+
+# displaying the square root
+print("The square root of", num, "is", sqrt)
