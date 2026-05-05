0b4f0257e5435611f656e337a6ae2650cfe17290
Shakil Ur Rehman <sanjaralap@gmail.com>
2026-05-05 07:27:09 +0500

    task addition

---
+++ addition.py
@@ -0,0 +1,10 @@
+# addition of two numbers by taking input from the user
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# adding two numbers
+sum = num1 + num2
+
+# displaying the sum
+print("The sum of", num1, "and", num2, "is", sum)
